7f079450d2c45491b6672a294dd8bd55b8ebc27f
Yap Yong Heng <yongheng98@hotmail.com>
2021-05-01 14:18:43 +0800

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,14 +23,33 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(response => {
       // console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      /* 
+      error >>> HttpErrorResponse
+
+      HttpErrorResponse {headers: HttpHeaders, status: 401, statusText: "OK", url: "https://localhost:5001/api/account/login", ok: false, …}
+      error: "Invalid username"
+      headers: HttpHeaders {normalizedNames: Map(0), lazyUpdate: null, lazyInit: ƒ}
+      message: "Http failure response for https://localhost:5001/api/account/login: 401 OK"
+      name: "HttpErrorResponse"
+      ok: false
+      status: 401
+      statusText: "OK"
+      url: "https://localhost:5001/api/account/login"
+      __proto__: HttpResponseBase
+
+      Need to use error.error to get error message
+      */
+      this.toastr.error(error.error);
     });
   }
 
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
   // checking whether user has logged in before
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -25,6 +26,23 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     }, error => {
       console.log(error);
+      /* 
+      error >>> HttpErrorResponse
+
+      HttpErrorResponse {headers: HttpHeaders, status: 401, statusText: "OK", url: "https://localhost:5001/api/account/login", ok: false, …}
+      error: "Invalid username"
+      headers: HttpHeaders {normalizedNames: Map(0), lazyUpdate: null, lazyInit: ƒ}
+      message: "Http failure response for https://localhost:5001/api/account/login: 401 OK"
+      name: "HttpErrorResponse"
+      ok: false
+      status: 401
+      statusText: "OK"
+      url: "https://localhost:5001/api/account/login"
+      __proto__: HttpResponseBase
+
+      Need to use error.error to get error message
+      */
+      this.toastr.error(error.error);
     });
   }
 
